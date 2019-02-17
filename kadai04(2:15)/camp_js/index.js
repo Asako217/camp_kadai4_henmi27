cf0bdbbf208fb3b5a83ff24268c2cf94874f806a
邊見麻子 <hasako217@gmail.com>
2019-02-17 18:20:29 +0900

    test

---
+++ kadai04(2:15)/camp_js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#ex').fadeIn(4000);
+});
+
+$(document).ready(function(){
+    $('p').css('background', '#000');
+    $('p').css('color', '#fff');
+    $('#ex p:nth-child(even)').css('background', '#fff');
+});
+
+
